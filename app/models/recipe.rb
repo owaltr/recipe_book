d5a672f2a9b1211379fb40d2edcb9a3e1e0fee1b
Oliver Walter <oliver@walter.fun>
2023-01-10 11:11:44 +0100

    add recipe form with simple validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
   has_one_attached :image
+
+  validates :title, presence: { message: "darf nicht leer sein" }, length: { maximum: 200, message: "maximal 200 Zeichen" }
+  validates :author, presence: { message: "darf nicht leer sein" }
+  validates :description, presence: { message: "darf nicht leer sein" }
 end
